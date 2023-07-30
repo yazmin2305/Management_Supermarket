594ca3531672b663f764397cec435ece4a09c5f0
Yazmin Gutierrez <yavigutierrez@unicauca.edu.co>
2023-07-30 12:51:48 -0500

    estructura base

---
+++ src/test/java/com/projectmarket/ProjectMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
